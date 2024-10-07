2c9f41973b920f38d895ac1d2d21899d97c99158
rkdhs <gaons120@gmail.com>
2024-10-07 23:00:40 +0900

    1463

---
+++ Algorithm(C++)/1463.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; cin >> n;
+    vector<int> dp(n + 1); dp[1] = 0;
+
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = dp[i - 1] + 1; // 1À» »©´Â °æ¿ì
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
+
+    cout << dp[n] << endl;
+}
