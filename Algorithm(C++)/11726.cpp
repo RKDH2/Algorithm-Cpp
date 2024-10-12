1a57fa9044c752af893351f272818b1379845069
rkdhs <gaons120@gmail.com>
2024-10-12 23:01:58 +0900

    11726

--- Algorithm(C++)/11726.cpp
+++ Algorithm(C++)/11726.cpp
@@ -19,3 +19,24 @@ int main() {
 
     cout << dp[n] << '\n';
 }
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(0);
+//    cout.tie(0);
+//
+//    int n; cin >> n;
+//    vector<int> dp;
+//    dp.push_back(1);
+//    dp.push_back(2);
+//
+//    for (int i = 2; i < n; ++i) {
+//        dp.push_back((dp[i - 1] + dp[i - 2]) % 10007);
+//    }
+//
+//    cout << dp[n - 1] << '\n';
+//}
