5fa22eb0f660880df6d9a3ed9a6bbb3f56d7bdf8
rkdhs <gaons120@gmail.com>
2024-10-29 23:09:58 +0900

    10866

--- Algorithm(C++)/10866.cpp
+++ Algorithm(C++)/10866.cpp
@@ -18,7 +18,40 @@ int main() {
             Deque.push_front(num);
         }
         else if (com == "push_back") {
-
+            cin >> num;
+            Deque.push_back(num);
+        }
+        else if (com == "pop_front") {
+            if (!Deque.empty()) {
+                cout << Deque.front() << '\n';
+                Deque.pop_front();
+            }
+            else {
+                cout << -1 << '\n';
+            }
+        }
+        else if (com == "pop_back") {
+            if (!Deque.empty()) {
+                cout << Deque.back() << '\n';
+                Deque.pop_back();
+            }
+            else {
+                cout << -1 << '\n';
+            }
+        }
+        else if (com == "size") {
+            cout << Deque.size() << '\n';
+        }
+        else if (com == "empty") {
+            cout << (Deque.empty() ? 1 : 0) << '\n';
+        }
+        else if (com == "front") {
+            if (!Deque.empty()) cout << Deque.front() << '\n';
+            else cout << -1 << '\n';
+        }
+        else if (com == "back") {
+            if (!Deque.empty()) cout << Deque.back() << '\n';
+            else cout << -1 << '\n';
         }
     }
 }
