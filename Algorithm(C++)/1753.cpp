3074722c485f759eaab1c3d6f5815b8231dd1c7f
unbreakable <gaons120@gmail.com>
2025-05-24 20:17:58 +0900

    1753, 2468

--- Algorithm(C++)/1753.cpp
+++ Algorithm(C++)/1753.cpp
@@ -12,9 +12,6 @@ int da[20001];
 int v, e, k;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
     cin >> v >> e >> k;
     fill(da, da + v + 1, INF);
     for (int i = 0; i < e; ++i) {
@@ -24,23 +21,11 @@ int main() {
     }
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     da[k] = 0;
-    pq.push({ da[k], k }); // 가중치, 정점
+    pq.push({ da[k], k }); // 정점 위치, 거리
     while (!pq.empty()) {
         auto cur = pq.top();
         pq.pop();
         if (da[cur.Y] != cur.X) continue;
-        for (auto nxd : vc[cur.Y]) {
-            if (da[nxd.Y] <= da[cur.Y] + nxd.X) continue;
-            da[nxd.Y] = da[cur.Y] + nxd.X;
-            pq.push({ da[nxd.Y], nxd.Y });
-        }
-    }
-    for (int i = 1; i <= v; ++i) {
-        if (da[i] == INF) {
-            cout << "INF" << '\n';
-        }
-        else {
-            cout << da[i] << '\n';
-        }
+
     }
 }
